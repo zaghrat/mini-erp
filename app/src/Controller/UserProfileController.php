cf3dc7b43b3918215736b7e5c0396d4727401333
Ahmed Zaghrat <zaghrat.ahmed@gmail.com>
2023-04-05 15:13:41 +0200

    SMTP DNS -- email sending

--- app/src/Controller/UserProfileController.php
+++ app/src/Controller/UserProfileController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use SymfonyCasts\Bundle\ResetPassword\Exception\ResetPasswordExceptionInterface;
@@ -28,14 +29,48 @@ class UserProfileController extends AbstractController
 
 
     #[Route('/user/profile/edit', name: 'app_user_profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $userPasswordHasher
+    ): Response
     {
+        /** @var User $user */
         $user = $this->getUser();
-        $form = $this->createForm(UserProfileType::class, $user, ['disabled_fields' => false]);
+        $form = $this->createForm(UserProfileType::class, $user, [
+            'disabled_fields' => false,
+            'show_password_field' => true
+        ]);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $user = $form->getData();
+            $user
+                ->setFirstName($form->get('firstName')->getData())
+                ->setLastName($form->get('lastName')->getData())
+                ->setEmail($form->get('email')->getData())
+            ;
+
+            $currentPassword = $form->get('currentPassword')->getData();
+            if (!empty($currentPassword)) {
+                // 1. verify if current password is valid
+                if ($userPasswordHasher->isPasswordValid($user, $currentPassword)) {
+                    $password = $form->get('password')->getData();
+                    $confirmPassword = $form->get('confirmPassword')->getData();
+                    // 2. verify if password and confirm password are identical
+                    if ($password === $confirmPassword) {
+                        $user->setPassword(
+                            $userPasswordHasher->hashPassword(
+                                $user,
+                                $password
+                            )
+                        );
+                    }
+                } else {
+                    return $this->render('user_profile/edit.html.twig', [
+                        'form'          =>  $form->createView()
+                    ], new Response(null, Response::HTTP_BAD_REQUEST));
+                }
+            }
 
             $entityManager->persist($user);
             $entityManager->flush();
